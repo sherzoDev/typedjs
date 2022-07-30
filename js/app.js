f585269349674bb6b972243b938c206910500085
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-07-31 00:05:55 +0500

    Intial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let options = {
+    strings: ["O'zbekiston", "Ibrohim", "Parij"],
+    typeSpeed: 40,
+    typeSpeed: 60,
+    startDelay: 000,
+    backSpeed: 60,
+    loop: true,
+    loopCount: Infinity,
+};
+
+let typed = new Typed('.type', options);
